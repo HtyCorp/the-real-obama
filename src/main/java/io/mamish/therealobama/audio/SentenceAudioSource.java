c495a2d7b107f0f52b0564e689e5934b0ff52710
Hamish Tym <hamishtym@bigpond.com>
2022-06-21 01:28:17 +1000

    Add temporary version of pause frames in audio source

--- src/main/java/io/mamish/therealobama/audio/SentenceAudioSource.java
+++ src/main/java/io/mamish/therealobama/audio/SentenceAudioSource.java
@@ -9,11 +9,14 @@ import java.util.concurrent.CompletableFuture;
 
 public class SentenceAudioSource extends AudioSourceBase {
 
+    private static final int NUM_PAUSE_FRAMES_BETWEEN_WORDS = 4; // x20ms = 80ms
+
     private final Sentence sentence;
     private final CompletableFuture<Void> onLastFrameFuture;
 
     private Word currentWord;
     private OpusFrame nextFrame;
+    private int numPauseFramesRemaining = 0;
 
     public SentenceAudioSource(DiscordApi api, Sentence sentence, CompletableFuture<Void> onLastFrameFuture) {
         super(api);
@@ -27,6 +30,7 @@ public class SentenceAudioSource extends AudioSourceBase {
         // As long as the current word is empty and there are more words to poll, advance through them
         while (currentWord.getOpusFrames().isEmpty() && !sentence.getWords().isEmpty()) {
             currentWord = sentence.getWords().remove();
+            numPauseFramesRemaining = NUM_PAUSE_FRAMES_BETWEEN_WORDS;
         }
 
         // Get next frame in the current word; this can be null if we're at the least word and have already polled all its frames
@@ -37,7 +41,7 @@ public class SentenceAudioSource extends AudioSourceBase {
     public byte[] getNextFrame() {
         byte[] frameData = Objects.requireNonNull(nextFrame, "Cannot get next frame when at end of stream").getData();
         advance();
-        if (!hasNextFrame() && !onLastFrameFuture.isDone()) {
+        if (nextFrame == null && !onLastFrameFuture.isDone()) {
             onLastFrameFuture.complete(null);
         }
         return frameData;
@@ -45,7 +49,17 @@ public class SentenceAudioSource extends AudioSourceBase {
 
     @Override
     public boolean hasNextFrame() {
+        if (numPauseFramesRemaining > 0) {
+            numPauseFramesRemaining--;
+            return false;
+        }
         return nextFrame != null;
+    }
+
+    @Override
+    public boolean hasFinished() {
+        // This method has to be overridden since we want to inject pauses using hasNextFrame without marking the source as finished (default impl)
+        return nextFrame == null;
     }
 
     @Override
